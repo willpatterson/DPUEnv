7452af0e545683ad236e8f7aa9e6b60635c7cc16
Will Patterson <wpatt2@pdx.edu>
2016-07-04 02:00:33 -0700

    added pyenv install

--- configure.sh
+++ configure.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# Configure script ##############
+#
+# Should be run after make deploy
+#################################
+
+#Install Pyenv
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+
 #Install Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+pyenv install 2.7.9
+pyenv install 3.5.1
 
 #Install HomeBrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
