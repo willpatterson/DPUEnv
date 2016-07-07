a0d4c05744c7a4737f45bcc737aa8e41d450c047
Will Patterson <wpatt2@pdx.edu>
2016-07-06 17:30:14 -0700

    Extended pyenv brew and cask installs

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Configure script ##############
+# Configure script for mac##############
 #
 # Should be run after make deploy
 #################################
@@ -11,10 +11,32 @@ git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 #Install Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-pyenv install 2.7.9
-pyenv install 3.5.1
+PYTHON_VERSIONS=(2.7.9 3.4.3 3.5.1)
+for v in $PYTHON_VERSIONS
+do
+    pyenv install $v 
+    pyenv shell $v
+    pip install --upgrade pip
+    pip install --upgrade setuptools
+    pip install virtualenv
+done
 
 #Install HomeBrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install cask
+BREW_INSTALLS=(cask tmux vim git wget ansible stow screenfetch htop)
+for b in $BREW_INSTALLS
+do
+    brew install $b
+done
+
+CASK_INSTALLS=(google-chrome slack virtualbox julia rust vlc atom)
+for c in $CASK_INSTALLS
+do
+    brew cask install $c
+done
+
+#Setup DevBench
+mkdir -p ~/DevBench
+
+#PROJECTS=(DkMonitor LASubway LogoBatch DotFiles)
