70381b42fe5f4808d8d5e13d022926a4b85ba717
Will Patterson <wpatt2@pdx.edu>
2017-03-21 02:29:26 -0700

    Added comments about issues from first run

--- configure.sh
+++ configure.sh
@@ -6,14 +6,14 @@
 # Should be run after make deploy
 ###############################################################################
 
-#Install Oh-my-zsh
+#Install Oh-my-zsh Problem: this opens a new shell, requires user to type exit to continue
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #Install Pyenv
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 
 PYTHON_VERSIONS=(2.7.9 3.4.3 3.5.1)
-for v in $PYTHON_VERSIONS
+for v in $PYTHON_VERSIONS #Doesnt work
 do
     pyenv install $v 
     pyenv shell $v
@@ -27,13 +27,13 @@ done
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 BREW_INSTALLS=(cask tmux vim git wget ansible stow screenfetch htop)
-for b in $BREW_INSTALLS
+for b in $BREW_INSTALLS #Only installs first entry
 do
-    brew install $b
+    brew install $b 
 done
 
 CASK_INSTALLS=(google-chrome slack virtualbox julia rust vlc atom)
-for c in $CASK_INSTALLS
+for c in $CASK_INSTALLS #Only installs first entry
 do
     brew cask install $c
 done
