4974401f20f5ae110b0ce711569e285cee037084
willpatterson <wpatt2@pdx.edu>
2017-11-26 14:49:09 -0800

    CREATE update function in dpuenv.sh

--- dpuenv.sh
+++ dpuenv.sh
@@ -2,10 +2,21 @@
 
 ACTION=$1 #build, pull, update
 PROFILE=$2
+COMMENTS=$3 #Optional commit message when updating
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DFILES=$(find $SCRIPT_DIR/profiles/$PROFILE/dotfiles -type f -exec basename {} \;)
 
 pull() {
-    DFILES=$(find $SCRIPT_DIR/profiles/$PROFILE/dotfiles -type f -exec basename {} \;)
     for f in $DFILES; do cp $SCRIPT_DIR/profiles/$PROFILE/dotfiles/$f ~/dpuvtest/.$f; done
+}
+
+update() {
+    for f in $DFILES; do 
+        cp ~/dpuvtest/.$f $SCRIPT_DIR/profiles/$PROFILE/dotfiles/$f
+    done
+    cd $SCRIPT_DIR
+    git add $SCRIPT_DIR/profiles/$PROFILE/dotfiles/
+    git commit -m "$COMMENTS"
+    git push
 }
